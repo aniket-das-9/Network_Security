c6f89e8f837769a189ca6251ad70d3df0d9ab180
aniket-das-9 <aniketdas092000@gmail.com>
2024-09-03 22:22:28 -0500

    exception, logger and get_data updated

--- get_data.py
+++ get_data.py
@@ -3,9 +3,13 @@ import sys
 import json
 
 from dotenv import load_dotenv
-
+load_dotenv()
+MONGO_DB_URL = os.getenv("MONGO_DB_URL")
+print(MONGO_DB_URL)
 
 import certifi
+ca = certifi.where()
+
 import numpy as np
 import pandas as pd
 import pymongo
