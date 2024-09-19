e7010ebf0164fb17f97bb4bbfdcfd494022d55d1
aniket-das-9 <aniketdas092000@gmail.com>
2024-09-19 13:42:34 -0500

    utils model and metric folders for model trainer

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -40,9 +40,12 @@ class TrainingPipeline:
         try:
             self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
             logging.info("Starting Data Ingestion.")
+            
             data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
+            
             data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
             logging.info(f"Data ingestion completed and artifact: {data_ingestion_artifact}")
+            
             return data_ingestion_artifact
 
 
@@ -52,8 +55,11 @@ class TrainingPipeline:
     def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact):
         try:
             data_validation_config = DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
+            
             data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,data_validation_config=data_validation_config)
+            
             data_validation_artifact=data_validation.initiate_data_validation()
+            
             return data_validation_artifact
         
         except Exception as e:
@@ -62,17 +68,28 @@ class TrainingPipeline:
     def start_data_transformation(self,data_validation_artifact:DataValidationArtifact):
         try:
             data_transformation_config = DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
+
             data_transformation = DataTransformation(data_transformation_config=data_transformation_config,
                                data_validation_artifact=data_validation_artifact)
+            
             data_transformation_artifact = data_transformation.initiate_data_transformation()
+            
             return data_transformation_artifact
         
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def start_model_trainer(self):
+    def start_model_trainer(self,data_transformation_artifact:DataTransformationArtifact)->ModelTrainerArtifact:
         try:
-            pass
+            model_trainer_config = ModelTrainerConfig(training_pipeline_config=self.training_pipeline_config)
+
+            model_trainer = ModelTrainer(model_trainer_config=model_trainer_config,
+                         data_transformation_artifact=data_transformation_artifact)
+            
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            
+            return model_trainer_artifact
+        
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
@@ -95,6 +112,7 @@ class TrainingPipeline:
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             # print(data_validation_artifact)
             data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
-            print(data_transformation_artifact)
+            # print(data_transformation_artifact)
+            model_trainer_artifact = self.start_model_trainer()
         except Exception as e:
             raise NetworkSecurityException(e,sys)
