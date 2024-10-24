bc231c0ebfe2bb40dbf235da1a0d76f9d5af4e57
aniket-das-9 <aniketdas092000@gmail.com>
2024-10-24 16:04:10 -0500

    workflow updated

--- airflow/dags/batch_prediction.py
+++ airflow/dags/batch_prediction.py
@@ -1,4 +1,4 @@
-"""
+'''
 from asyncio import tasks
 import json
 from textwrap import dedent
@@ -61,4 +61,4 @@ with DAG(
     )
 
     download_input_files >> generate_prediction_files >> upload_prediction_files
-    """
+    '''
